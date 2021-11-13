ee718c7857ad4942e565399486ec3893e8ecac7f
23042304 <fomenko_ak21@nuwm.edu.ua>
2021-11-13 19:55:50 +0200

    release v1

---
+++ My Firsh Game/My Tirsh Game/MyFirstGame.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string.h>
+#include "ClassPlayer.h"
+#include "ClassHero.h"
+#include "HeroManager.h"
+#include "PlayerManager.h"
+#include "LogicFunction.h"
+
+using namespace std;
+
+int main() {
+
+}
